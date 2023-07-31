0c2a88dd7b84be039dea03f64f78dafef67dc68f
Nikolay Litvinenko <Litvinenko_n.v@mail.ru>
2023-07-31 21:40:27 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import styles from "./App.module.sass";
 
 function App() {
-  return <div className={styles.wrapper}></div>;
+  return <div className={styles.wrapper}>Ghbdtn</div>;
 }
 
 export default App;
