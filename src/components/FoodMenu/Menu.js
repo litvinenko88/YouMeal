4d49a8da2886778cac781ae7edaa44abcb8b3d22
Nikolay Litvinenko <Litvinenko_n.v@mail.ru>
2023-08-13 22:30:34 +0300

    Переключение меню

--- src/components/FoodMenu/Menu.js
+++ src/components/FoodMenu/Menu.js
@@ -1,9 +1,36 @@
 import styles from "./Menu.module.css";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { MenuContext } from "../../context/MenuContext";
+import axios from "axios";
+import MenuList from "./MenuList";
 
 function Menu() {
   const data = useContext(MenuContext);
+
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(data.menu[data.activ - 1].link)
+      .then((response) => {
+        setPost(response.data);
+      })
+      .catch((error) => console.error(error));
+  }, [data.activ]);
+
+  return (
+    <ul className={styles.menu}>
+      {post.map((item) => (
+        <MenuList
+          key={item.id}
+          img={item.img}
+          price={item.price}
+          title={item.title}
+          weight={item.weight}
+        />
+      ))}
+    </ul>
+  );
 }
 
 export default Menu;
