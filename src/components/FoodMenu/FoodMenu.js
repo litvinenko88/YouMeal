c044f536170459268b096c10ac38abcaf6c61e5e
Nikolay Litvinenko <Litvinenko_n.v@mail.ru>
2023-08-14 22:46:00 +0300

    commit

--- src/components/FoodMenu/FoodMenu.js
+++ src/components/FoodMenu/FoodMenu.js
@@ -1,9 +1,11 @@
+import styles from "./FoodMenu.module.css";
+
 import Basket from "./Basket";
 import Menu from "./Menu";
 
 function FoodMenu() {
   return (
-    <div>
+    <div className={styles.wrapper}>
       <Basket />
       <Menu />
     </div>
